ec956db496d66bf3bd91ff068e132ab3a26c4ff3
Wilian F. Correa Souza <wilian.silva3@fatec.sp.gov.br>
2024-04-16 16:56:13 -0300

    Acessando índices de um array

---
+++ js/02.arrays.js
@@ -0,0 +1,23 @@
+'use strict'
+
+// console.log("Arrays!");
+
+
+//DECLARANDO UM ARRAY
+const alunos = ["Wilian", "Vinícius", "Marly", "Duda", "Gabriel"]
+
+//Exibindo a estrutura do array (somente no console)
+console.log(alunos);
+
+//Acessando indicidualmente algum elemento através de sua posição no array([indice])
+console.log(`Aluno n° 2 ► ${alunos[1]}`);
+console.log(`Aluno n° 5 ► ${alunos[4]}`);
+
+/* ATIVIDADE */
+
+/*- Criar novo array com nome de 7 coisas que gosta*/
+
+let sobremesa = ["Brigadeiro", "Sorvete", "Paçoca", "Mousse", "Goiabada", "Pavê", "Bolo"];
+
+/*- Mostrar uma frase personalizada com o 2° nome, 5° nome e 7° nome do array */
+console.log(`Gosto muido de ${sobremesa[1]} misturado com ${sobremesa[4]} e um pedaço de ${sobremesa[6]}!`);
