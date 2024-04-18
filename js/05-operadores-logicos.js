d27d61a4778d64bdf84f2bc3c647990b670ace7c
Wilian F. Correa Souza <wilian.silva3@fatec.sp.gov.br>
2024-04-18 15:20:56 -0300

    Operador lógico && (AND)

--- js/05-operadores-logicos.js
+++ js/05-operadores-logicos.js
@@ -1,2 +1,22 @@
 'use strict'
 
+// console.log("Olá");
+
+// Escola - Requisitos para aprovação (media 7 e presença)
+
+const mediaMinima = 7;
+const limiteDeFaltas = 10;
+
+//Aluno
+let aluno= "Dayane";
+let mediaFinal= 8;
+let faltas= 7;
+let redultado;
+
+/*  UTILIZANDO OPERADOR LÓGICO && (AND) - resultado verdadeiro apenas se contemplar os dois requisitos */
+
+if (mediaFinal >= mediaMinima && faltas <= limiteDeFaltas){
+    console.log("Aprovada!");
+} else {
+    console.log("Reprovada!");
+}
